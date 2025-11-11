771f80ad82e8ab27d73951c2e4c387fa5263eb5a
TS <tinusunny04@gmail.com>
2025-11-11 15:01:09 +0530

    All-Book-Page-desg updated

--- src/Users/pages/AllBooks.jsx
+++ src/Users/pages/AllBooks.jsx
@@ -13,67 +13,107 @@ function AllBooks() {
   
       {/* Heading */}
   
-         <div>
-          <h2 className='text-center text-4xl font-bold italic m-10'>Collections</h2>
-          <div className='flex  justify-center ' style={{width:"100%"}}>
-            <TextInput  style={{backgroundColor:'white', width:'500px', color:'black'}}/>
-              <Button className='ml-1'>
-                <FaSearch className="m-2 h-4 w-4 " />
+         <div className='text-center mt-10 px-4'>
+          <h2 className=' text-3xl md:text-4xl 0 font-bold italic m-8'>Collections</h2>
+
+          {/* search bar */}
+          <div className='flex  justify-center item-center '>
+            <TextInput className='w-64 sm:w-80 md:w-[500px] bg-white text-black' placeholder='Search Books ...'/>
+              <Button className='ml-2'>
+                <FaSearch className=" h-4 w-4 " />
         </Button>
           </div>
           </div>    
+  {/* main content */}
+         
+  <div className='flex flex-col md:flex-row mt-10 justify-start item-start gap-6 px-4'>
+     {/* Side bar */}
+         <div className="w-full md:w-1/4 md:sticky md:top-20 p-4   rounded-lg h-50 mb-50">
+          <h3 className='text-lg font-semibold mb-4'>Filters</h3>
+
+           <div className=' space-y-3 mb-4'>
+               <div className="flex items-center gap-2  m-4">
+        <Radio id="No-filter" name="countries" value="No-filter" defaultChecked />
+        <Label htmlFor="No-filter" >No-filter</Label>
+      </div>
+      <div className="flex items-center gap-2  m-4 text-black">
+        <Radio id=" Literary Fiction" name="countries" value=" Literary Fiction" />
+        <Label htmlFor=" Literary Fiction"> Literary Fiction</Label>
+      </div>
+      <div className="flex items-center  m-4 gap-2">
+        <Radio id="Philosophy" name="countries" value="Philosophy" />
+        <Label htmlFor="Philosophy">Philosophy</Label>
+      </div>
+       <div className="flex items-center  m-4 gap-2">
+        <Radio id="Thriller" name="countries" value="Thriller" />
+        <Label htmlFor="Thriller">Thriller</Label>
+      </div>
+       <div className="flex items-center m-4 gap-2">
+        <Radio id="Romance" name="countries" value="Romance" />
+        <Label htmlFor="Romance">Romance</Label>
+      </div>
+       <div className="flex items-center m-4 gap-2">
+        <Radio id="Horror" name="countries" value="Horror" />
+        <Label htmlFor="Horror">Horror</Label>
+      </div>
   
-          {/* Side bar */}
-  <div className='m-10'>
-         <div className="flex max-w-md flex-col gap-4 bg- p-6 rounded-lg text-black" style={{backgroundAttachment:'fixed'}}>
-          <h3>Filters</h3>
-
-             <div className="flex items-center gap-2">
-      <Radio id="No-filter" name="countries" value="No-filter" defaultChecked />
-      <Label htmlFor="No-filter">No-filter</Label>
-    </div>
-    <div className="flex items-center gap-2 text-black">
-      <Radio id=" Literary Fiction" name="countries" value=" Literary Fiction" />
-      <Label htmlFor=" Literary Fiction"> Literary Fiction</Label>
-    </div>
-    <div className="flex items-center gap-2">
-      <Radio id="Philosophy" name="countries" value="Philosophy" />
-      <Label htmlFor="Philosophy">Philosophy</Label>
-    </div>
-     <div className="flex items-center gap-2">
-      <Radio id="Thriller" name="countries" value="Thriller" />
-      <Label htmlFor="Thriller">Thriller</Label>
-    </div>
-     <div className="flex items-center gap-2">
-      <Radio id="Romance" name="countries" value="Romance" />
-      <Label htmlFor="Romance">Romance</Label>
-    </div>
-     <div className="flex items-center gap-2">
-      <Radio id="Horror" name="countries" value="Horror" />
-      <Label htmlFor="Horror">Horror</Label>
-    </div>
-
-       <div className="flex items-center gap-2">
-      <Radio id="Auto/Biography" name="countries" value="Auto/Biography" />
-      <Label htmlFor="Auto/Biography">Auto/Biography</Label>
-    </div>
-       <div className="flex items-center gap-2">
-      <Radio id="Self-Help" name="countries" value="Self-Help" />
-      <Label htmlFor="Self-Help">Self-Help</Label>
-    </div>
-       <div className="flex items-center gap-2">
-      <Radio id="Politics" name="countries" value="Politics" />
-      <Label htmlFor="Politics">Politics</Label>
-    </div>
+         <div className="flex items-center m-4 gap-2">
+        <Radio id="Auto/Biography" name="countries" value="Auto/Biography" />
+        <Label htmlFor="Auto/Biography">Auto/Biography</Label>
+      </div>
+         <div className="flex items-center  m-4 gap-2">
+        <Radio id="Self-Help" name="countries" value="Self-Help" />
+        <Label htmlFor="Self-Help">Self-Help</Label>
+      </div>
+         <div className="flex items-center  m-4 gap-2">
+        <Radio id="Politics" name="countries" value="Politics" />
+        <Label htmlFor="Politics">Politics</Label>
+      </div>
+           </div>
     
    
 
   </div>
 
-<div className=' flex justify-evenly gap-5   flex-wrap'>
+  {/* books Grid */}
+
+<div className='w-full md:w-3/4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8'>
 
 
-<Card className='w-80  p-0' style={{backgroundColor:'white', marginTop:'-500' }}>
+<Card className='w-80  p-0' style={{backgroundColor:'white'}}>
+
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJNXLI4-WxfgDBBiymHzxj-ugg_ZRfyomA-g&s" alt="book-img" className='h-70' width='100%' />
+
+<h4>Book Name</h4>
+<p>type</p>
+<p>$</p>
+<Button>Know More</Button>
+</Card>
+
+<Card className='w-80  p-0' style={{backgroundColor:'white'}}>
+
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJNXLI4-WxfgDBBiymHzxj-ugg_ZRfyomA-g&s" alt="book-img" className='h-70' width='100%' />
+
+<h4>Book Name</h4>
+<p>type</p>
+<p>$</p>
+<Button>Know More</Button>
+</Card>
+
+<Card className='w-80  p-0' style={{backgroundColor:'white'}}>
+
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJNXLI4-WxfgDBBiymHzxj-ugg_ZRfyomA-g&s" alt="book-img" className='h-70' width='100%' />
+
+<h4>Book Name</h4>
+<p>type</p>
+<p>$</p>
+<Button>Know More</Button>
+</Card>
+
+<Card className='w-80  p-0' style={{backgroundColor:'white'}}>
 
 
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJNXLI4-WxfgDBBiymHzxj-ugg_ZRfyomA-g&s" alt="book-img" className='h-70' width='100%' />
@@ -117,7 +157,7 @@ function AllBooks() {
 </div>
 </div>
 
-   <AppFooter/>   
+  <div> <AppFooter/>   </div>
 
     </>
   )
