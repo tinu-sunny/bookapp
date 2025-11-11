209ab1e3cb938f0bb61dae7e36243a306b6e2ff8
TS <tinusunny04@gmail.com>
2025-11-12 03:11:02 +0530

    Adminn-home page desg -updateted

--- src/admin/components/AdminHeader.jsx
+++ src/admin/components/AdminHeader.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import { Link } from "react-router-dom";
 
 function AdminHeader() {
   return (
-    <div>AdminHeader</div>
+   <>
+      <Navbar fluid rounded>
+      <NavbarBrand href="#" className="ml-20">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHndl4ELH7r0vrtGgKUOMSsjIvWXDcjKIdQQ&s" className="mr-3 h-6 sm:h-9" alt="F" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Book Finder</span>
+      </NavbarBrand>
+      <div className="flex md:order-2 mr-20">
+        <Button>LogOut</Button>
+        <NavbarToggle />
+      </div>
+   
+    </Navbar>
+   </>
   )
 }
 
--- src/admin/pages/AdminHome.jsx
+++ src/admin/pages/AdminHome.jsx
@@ -1,8 +1,87 @@
 import React from 'react'
+import AdminHeader from '../components/AdminHeader'
+import AppFooter from '../../components/AppFooter'
+import { Card } from 'flowbite-react'
+import { Sidebar, SidebarCollapse, SidebarItem, SidebarItemGroup, SidebarItems } from "flowbite-react";
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser } from "react-icons/hi";
 
 function AdminHome() {
   return (
-    <div>AdminHome</div>
+    <>
+    <AdminHeader/>
+
+    {/* home page  */}
+
+    <div className='flex justify-start gap-20 mb-20 mt-10'>
+      {/* side slider */}
+      <div>
+       <Sidebar aria-label="Sidebar with multi-level dropdown example">
+      <SidebarItems>
+        <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt=""  width={'100px'} height={'100px'}/>
+        <h6 className='text-star'>Admin </h6>
+        <SidebarItemGroup>
+        
+          <SidebarItem href="#">
+            Home
+          </SidebarItem>
+          <SidebarItem href="#" >
+            All-Books
+          </SidebarItem>
+          <SidebarItem href="#" >
+          Careers
+          </SidebarItem>
+          <SidebarItem href="#" >
+            Settings
+          </SidebarItem>
+         
+        </SidebarItemGroup>
+      </SidebarItems>
+    </Sidebar>
+      </div>
+      {/* charts */}
+      <div>
+{/* cards */}
+        <div className='flex w-full justify-evenly gap-6'>
+<Card  className='text-center bg-green-500'>
+  <p>icon</p>
+  <p>Total Number Of Books <span>100+ </span></p>
+</Card>
+<Card className='text-center bg-blue-500' >
+  <p>icon</p>
+  <p>Total Number Of User <span>100+ </span></p>
+</Card>
+<Card className='text-start bg-orange-500'>
+  <p>icon</p>
+  <p>Total Number Of Employees <span>100+ </span></p>
+</Card>
+        </div>
+
+        {/* charts */}
+
+        <div className='flex justify-center gap-5 m-5 '>
+          {/* chart -1 */}
+          <div className='w-full'>
+          <Card> 
+            <p>snskh</p>
+          </Card>
+          </div>
+          {/* chart-2 */}
+           <div className='w-full'>
+          <Card> 
+            <p>snskh</p>
+          </Card>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+
+
+
+    <AppFooter/>  
+    </>
   )
 }
 
